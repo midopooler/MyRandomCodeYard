c0822ca86782baa7c1e0ecbd5fa68eefaf67edda
Pulkit Midha <midhapulkit28@gmail.com>
2020-03-10 01:30:45 +0530

    Add files via upload

---
+++ RotateModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateModel : MonoBehaviour {
+    public Slider slider;
+    public GameObject model;
+
+    public void SliderRotatingModel()
+    {
+            model.transform.rotation = Quaternion.Euler(model.transform.rotation.x, slider.value, model.transform.rotation.z);
+    }
+    
+}
