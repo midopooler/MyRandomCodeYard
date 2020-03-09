2c15b36ea9f2f253443e01f2de9fae07c0b273e9
Pulkit Midha <midhapulkit28@gmail.com>
2020-03-10 01:31:12 +0530

    Add files via upload

---
+++ SCENEMANAGER.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SCENEMANAGER : MonoBehaviour
+{
+    public void SceneLoader(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
