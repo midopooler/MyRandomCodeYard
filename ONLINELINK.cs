e6ec8e1d3bfafba83557ed0ef155d90e4d00f109
Pulkit Midha <midhapulkit28@gmail.com>
2020-03-10 22:22:07 +0530

    Add files via upload

---
+++ ONLINELINK.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ONLINELINK : MonoBehaviour
+{
+
+    public void buys()
+    {
+        Application.OpenURL("https://www.amazon.in/b?ie=UTF8&node=7355550031");
+    }
+
+}
